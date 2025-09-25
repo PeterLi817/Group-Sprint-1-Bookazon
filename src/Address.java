b1385162c8e91c7fcadbadaa3fe1d8ca65630fe4
PeterLi817 <plpeter817@gmail.com>
2025-09-25 11:41:34 -0400

    Added address class

---
+++ src/Address.java
@@ -0,0 +1,78 @@
+public class Address {
+    private int addressNumber; 
+    private String street;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String country;
+
+    // Default constructor
+    public Address() {
+    }
+
+    // Full constructor
+    public Address(int addressNumber, String street, String city, String state, int zipCode, String country) {
+        this.addressNumber = addressNumber;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public int getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(int addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return addressNumber + " " + street + ", " + city + ", " + state + " " + zipCode + ", " + country;
+    }
+
+}

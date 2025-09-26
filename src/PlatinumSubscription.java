71697c7fec8c9f78652d03c33c9fddcefcbacf5d
PeterLi817 <plpeter817@gmail.com>
2025-09-26 11:22:43 -0400

    refactored classes that rely on subscription to use the new classes

--- src/PlatinumSubscription.java
+++ src/PlatinumSubscription.java
@@ -1,7 +1,8 @@
 public class PlatinumSubscription extends Subscription{
     public static final double discountRate = 0.10;
+    public static final String subscriptionName = "Platinum";
 
     public PlatinumSubscription() {
-        super(discountRate);
+        super(discountRate,subscriptionName);
     }
 }
--- src/Subscription.java
+++ src/Subscription.java
@@ -1,11 +1,16 @@
 abstract class Subscription {
     private double discout;
+    private String name;
 
-    Subscription(double discout) {
+    Subscription(double discout, String name) {
         this.discout = discout;
     }
 
     public double getDiscout() {
         return discout;
+    }
+
+    public String getName() {
+        return name;
     }
 }
